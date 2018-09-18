7dfeced0906458346d8cfcc64856f392f4e3097e
Darío M <dario.aviles@gmail.com>
2018-09-18 14:45:08 +0200

    Fix code format

--- server.py
+++ server.py
@@ -4,7 +4,7 @@
 # @Date:   2018-09-18T09:36:04+02:00
 # @Email:  dario.aviles@gmail.com
 # @Last modified by:   Darío M. García Carretero
-# @Last modified time: 2018-09-18T09:37:12+02:00
+# @Last modified time: 2018-09-18T14:41:03+02:00
 
 
 import sys
@@ -20,20 +20,20 @@ import client
 
 class Command(object):
 
-    def __init__(self,m):
-        self.m=m
+    def __init__(self, m):
+        self.m = m
 
-    def run(self,server,clientsocket,*args,**kwargs):
+    def run(self, server, clientsocket, *args, **kwargs):
         raise NotImplementedError()
 
 
 class ReplyCommand(Command):
 
-    def run(self,server,clientsocket,*args,**kwargs):
+    def run(self, server, clientsocket, *args, **kwargs):
 
         try:
-            li=len(self.m.encode(server.encoding))
-            clientsocket.send(li.to_bytes(server.max_bytes_len,sys.byteorder)+bytearray(self.m,server.encoding))
+            li = len(self.m.encode(server.encoding))
+            clientsocket.send(li.to_bytes(server.max_bytes_len, sys.byteorder)+bytearray(self.m, server.encoding))
             clientsocket.close()
         except ConnectionResetError: #Remote hosts disconnected
             pass
@@ -41,42 +41,42 @@ class ReplyCommand(Command):
 
 class QuitCommand(ReplyCommand):
 
-    def __init__(self,m):
+    def __init__(self, m):
         super(QuitCommand, self).__init__('Shutting down server. Bye!')
 
 class HelloCommand(ReplyCommand):
 
-    def run(self,server,clientsocket,*args,**kwargs):
-        self.m= 'Hi there!, wellcome to {0} at port {1}'.format(server.host, server.port)
-        super(HelloCommand,self).run(server, clientsocket)
+    def run(self, server, clientsocket, *args, **kwargs):
+        self.m = 'Hi there!, wellcome to {0} at port {1}'.format(server.host, server.port)
+        super(HelloCommand, self).run(server, clientsocket)
 
 class ExitCommand(ReplyCommand):
 
-    def run(self,server,clientsocket,*args,**kwargs):
-        self.m= 'Thanks for the visit, come back soon!'
-        super(ExitCommand,self).run(server, clientsocket)
+    def run(self, server, clientsocket, *args, **kwargs):
+        self.m = 'Thanks for the visit, come back soon!'
+        super(ExitCommand, self).run(server, clientsocket)
 
 class TestCommand(ReplyCommand):
 
     def run(self,server,clientsocket,*args,**kwargs):
-        self.m= 'TESTING' #Return message
-        super(TestCommand,self).run(server,clientsocket)
+        self.m = 'TESTING' #Return message
+        super(TestCommand, self).run(server,clientsocket)
 
 # PUT YOURS COMMANDS HERE!
 
 
 class  Parser(object):
 
-    def transform_command(self,command):
+    def transform_command(self, command):
         return ''.join([x.capitalize() for x in command.split('_')])+'Command'
 
     def parse(self,s):
         thismodule = sys.modules[__name__]
-        c=s.split()
+        c = s.split()
         if len(c):
             command=ReplyCommand('Um? "%s". I don\'t know what you are talking about.' % s.strip())
             try:
-                command=getattr(thismodule,self.transform_command(c[0]))(' '.join(c[1:]))
+                command=getattr(thismodule, self.transform_command(c[0]))(' '.join(c[1:]))
             except AttributeError:
                 pass
         else:
@@ -88,44 +88,43 @@ class Server(object):
 
     def __init__(self,host,port,**kwargs):
 
-        self.host=host
-        self.port=port
-        self.kwargs=kwargs
-        self.encoding='utf8'
-        self.max_bytes_len=8
-        self.max_bytes_str='@Q'
-        self.max_command_len=pow(2,self.max_bytes_len*8)
+        self.host = host
+        self.port = port
+        self.kwargs = kwargs
+        self.encoding = 'utf8'
+        self.max_bytes_len = 8
+        self.max_bytes_str = '@Q'
+        self.max_command_len = pow(2, self.max_bytes_len*8)
 
 
     def serve(self):
 
-        parser=Parser()
-        command=None
-        serversocket = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)
+        parser = Parser()
+        command = None
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         serversocket.bind((host, port))
         serversocket.listen(5)
         while (type(command) is not QuitCommand):
             (clientsocket, address) = serversocket.accept()
-            l,=struct.unpack(self.max_bytes_str,clientsocket.recv(self.max_bytes_len))
-            m=clientsocket.recv(l).decode(self.encoding)
-            command=parser.parse(m)
-            threading.Thread(target=command.run,args=(self,clientsocket,),kwargs=self.kwargs).start()
+            l, = struct.unpack(self.max_bytes_str, clientsocket.recv(self.max_bytes_len))
+            m = clientsocket.recv(l).decode(self.encoding)
+            command = parser.parse(m)
+            threading.Thread(target=command.run, args=(self,clientsocket,), kwargs=self.kwargs).start()
 
 
 if __name__ == '__main__':
 
-    config=configparser.ConfigParser()
-    argument_parser=argparse.ArgumentParser()
+    config = configparser.ConfigParser()
+    argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument('config', help='Config file')
     args = argument_parser.parse_args()
-    config_path=os.path.realpath(args.config)
+    config_path = os.path.realpath(args.config)
     config.readfp(open(config_path))
-    host =config.get('general','host')
-    port=config.getint('general','port')
-    server=Server(host,port)
-    st=threading.Thread(target=server.serve)
+    host = config.get('general','host')
+    port = config.getint('general','port')
+    server = Server(host,port)
+    st = threading.Thread(target=server.serve)
     st.start()
     print('Started server at "{0}" in port "{1}". '.format(host,port),end='')
     print('Close this window or use "CTR+C" to stop server  ',end='',flush=True)
@@ -137,5 +136,5 @@ if __name__ == '__main__':
               sys.stdout.flush()
         except KeyboardInterrupt:
             client.Client(host,port).comunicate('QUIT')
-            print('\nSignal received finishing pending tasks  ',end='')
-    print('\nServer stopped',flush=True)
+            print('\nSignal received finishing pending tasks', end='')
+    print('\nServer stopped', flush=True)
